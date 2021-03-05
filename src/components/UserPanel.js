0cc5041fade1307ab424917b717c9172d5b6e7a4
guillermokatz <guillokatz@gmail.com>
2021-03-05 14:55:46 -0300

    api in proxy test

--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -103,7 +103,7 @@ function UserPanel () {
 
             <section className=" grid lg:grid-cols-3 grid-cols-1 gap-2 p-2 w-full">
 
-            {user === "" ? <p>CARGANDO CHAJAS...</p> : user.Chaja.map ( chaja => {
+            {user === "" ? <p className="font-bold">CARGANDO CHAJAS...</p> : user.Chaja.map ( chaja => {
             return <article key={chaja.id} className=" shadow-lg font-semibold m-2 bg-white rounded border-4 b-gray-900 py-2 px-4 text-xl text-gray-700 transition  hover:bg-purple-800 hover:text-white overflow-hidden">
                     <p className="inline-block">{chaja.chaja}</p>
                     <p className="pt-1 text-right text-sm">{new Date(chaja.createdAt).toLocaleString('en-GB', {day: '2-digit', month: '2-digit', year: '2-digit', hour: '2-digit', minute:'2-digit'})}</p>
